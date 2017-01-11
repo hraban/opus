165dcfb3708d15d4765120638d208e284e3d55c3
Hraban Luyat <hraban@0brg.net>
2017-01-11 00:46:08 +0000

    Update godoc on updated method names

--- encoder.go
+++ encoder.go
@@ -244,7 +244,7 @@ func (enc *Encoder) SetBitrate(bitrate int) error {
 	return nil
 }
 
-// SetBitrateAuto will allow the encoder to automatically set the bitrate
+// SetBitrateToAuto will allow the encoder to automatically set the bitrate
 func (enc *Encoder) SetBitrateToAuto() error {
 	res := C.bridge_encoder_set_bitrate(enc.p, C.opus_int32(C.CONST_BITRATE_AUTO))
 	if res != C.OPUS_OK {
@@ -253,7 +253,7 @@ func (enc *Encoder) SetBitrateToAuto() error {
 	return nil
 }
 
-// SetBitrateMax causes the encoder to use as much rate as it can. This can be
+// SetBitrateToMax causes the encoder to use as much rate as it can. This can be
 // useful for controlling the rate by adjusting the output buffer size.
 func (enc *Encoder) SetBitrateToMax() error {
 	res := C.bridge_encoder_set_bitrate(enc.p, C.opus_int32(C.CONST_BITRATE_MAX))
