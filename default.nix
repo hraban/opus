7ae8476db790934376e654cfe264fdde99aaadf6
Hraban Luyat <hraban@0brg.net>
2025-11-17 00:33:21 -0500

    build: default.nix for building and testing in nix

---
+++ default.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  inherit (pkgs) lib;
+in
+pkgs.buildGoModule {
+  name = "go-opus";
+  src = lib.cleanSource ./.;
+  buildInputs = with pkgs; [
+    libogg
+    libopus
+    opusfile
+  ];
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    moreutils
+  ];
+  checkFlags = "-race";
+  postCheck = ''
+    gofmt -d | tee /dev/stderr | ifne false
+  '';
+  vendorHash = null;
+}
